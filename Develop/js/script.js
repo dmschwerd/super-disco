96d42bb49b0c032464761675d18d8f0a7fee8609
David Schwerd <dmsch656@gmail.com>
2022-02-14 14:49:32 -0700

    stores fields in local storage

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,5 +1,5 @@
 var timeblocksEl = document.getElementById('time-blocks');
-
+var data = JSON.parse(localStorage.getItem('data')) || {};
 
 for(var i = 0; i < 13; i++) {
     var timeBlock = document.createElement('div');
@@ -7,8 +7,19 @@ for(var i = 0; i < 13; i++) {
     var timeInputEl = document.createElement('input');
     var timeSaveButton = document.createElement('button');
 
+    
+    timeSaveButton.setAttribute('data-hour', i);
+    timeInputEl.id = 'hour-' + i;
+    timeInputEl.value = data[i] || '';
     timeEl.textContent = i + ' hours';
     timeSaveButton.textContent = 'Save'
+    timeSaveButton.addEventListener('click', function(event) {
+        var hour = event.target.getAttribute('data-hour');
+        var input = document.getElementById('hour-' + hour)
+        var inputValue = input.value;
+        data[hour] = inputValue;
+        localStorage.setItem('data', JSON.stringify(data));
+    })
 
     timeBlock.appendChild(timeEl);
     timeBlock.appendChild(timeInputEl);
